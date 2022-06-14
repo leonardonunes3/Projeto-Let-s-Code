06e112e5beb05a052c251e9462bebc1a9e949ddc
F1685131 Breno Bezerra de Melo Alencar <breno.bezerraalencar@bb.com.br>
2022-06-14 14:23:22 -0300

    Corrige rendimentos de ContaPoupanca e impede que clientes PJ as criem.

--- src/main/java/br/letscode/models/ContaPoupanca.java
+++ src/main/java/br/letscode/models/ContaPoupanca.java
@@ -9,11 +9,20 @@ public class ContaPoupanca extends ContaSaldo {
 
     public ContaPoupanca(int numero, Pessoa cliente) {
         super(numero, cliente);
+        if(cliente instanceof PessoaJuridica){
+            throw new IllegalArgumentException("Pessoas jurídicas não podem abrir conta poupança.");
+        }
     }
 
     @Override
     public void processaRendimentos(Period periodo) {
         if(periodo.isNegative()) throw new IllegalArgumentException("Periodo não pode ser negativo");
-        this.depositar(RENDIMENTO_DIARIO.add(BigDecimal.ONE).pow(periodo.getDays()).subtract(BigDecimal.ONE));
+        this.depositar(
+                RENDIMENTO_DIARIO
+                    .add(BigDecimal.ONE)
+                    .pow(periodo.getDays())
+                    .subtract(BigDecimal.ONE)
+                    .multiply(this.getSaldo())
+        );
     }
 }
